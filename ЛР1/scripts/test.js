d05c9d6106c52011bd8ede79f7450f46ff36ccf3
VladislavKasatkin1 <144897888+VladislavKasatkin1@users.noreply.github.com>
2023-11-03 18:48:17 +0600

    Add files via upload

---
+++ ЛР1/scripts/test.js
@@ -0,0 +1,57 @@
+//alert('Привет');
+const mine = 10;
+//let c = 23189239214894128984129142 (bigInt)
+var a = 5;
+let b = 'строка';
+alert(b);
+//console.log(a);
+let name = "Ivan";
+//alert("Привет,"+name);
+//alert(`Привет, ${name}!`);
+let fieldTrue = true;
+let fieldFalse = false;
+
+//prompt("Введите число");
+
+/*let age = prompt("Сколько тебе лет?");
+if (age>=18) alert("Вы совершеннолетний");
+//else if (...)
+else alert("Вы несовершеннолетний"); 
+*/
+/* цикл while
+let i = 0;
+while (i<3) {
+console.log(i);
+i++;
+}
+*/
+
+/*
+let i = 0;
+do {
+console.log(i);
+i++;
+}
+*/
+
+/*
+for(i=0; i<3; i++){
+console.log(i);}
+*/
+
+switch(2){
+    case 2: console.log(2); break;
+    case 3: console.log(3); break;
+}
+
+function showMessage(){
+    alert("Hi");
+}
+function sum(a,b) {
+return console.log(a+b);
+}
+sum(5,2);
+
+//let arr = new Array();
+let arr2 = [];
+let fruits=['Яблоко', 'Апельсин', 'Груша'];
